738bfcf9db9c665051d7c8384d81cf9d0a993caf
nijuy <youjin6325@naver.com>
2023-08-06 18:41:11 +0900

    feat(Read): 글 읽기 기능 구현

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -3,8 +3,20 @@ interface ReadProps {
     id: string
   }
 }
-const Read = (props: ReadProps) => {
-  return <>Read: {props.params.id}</>
+const Read = async (props: ReadProps) => {
+  const response = await fetch(
+    `http://localhost:9999/topics/${props.params.id}`
+  )
+  const topic = await response.json()
+
+  return (
+    <>
+      <article>
+        <h2>{topic.title}</h2>
+        <p>{topic.body}</p>
+      </article>
+    </>
+  )
 }
 
 export default Read
